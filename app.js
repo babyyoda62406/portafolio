8a6afa15e2e02a1b0b58b16c78ddd8e616504c86
Deivis <deivis@debian.debian>
2023-09-23 16:22:24 -0400

    reset(DTM/Reset)

--- app.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config()
-console.clear()
-const Server = require("./models/server")
-const app = new Server(process.env.PORT)
-
--- routes/get.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require('express')
-const { getItems } = require('../controllers/get')
-
-const app = Router()
-
-
-app.get("/getItems" , getItems)
-
-module.exports = app 
